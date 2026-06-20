2fc8bf78bb3883e1345fc9cc3078c518d5f922b4
น้ำผึ้ง <Nampungdee5555@gmail.com>
2026-06-21 01:28:56 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,11 +1,1 @@
-// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App (สำหรับระบบสมาชิกรวม member)
-const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycby2cit0L6OOwnlWYwOu9Er--MDwEHqcJUnKUBdfNrHpIwfNepA8r01x11p9Q8NGwwQ1/exec';
 
-// *** ตัวแปรหลักที่ Report.html เรียกใช้ ***
-// นำ URL Web App ตัวใหม่ที่ได้จากการ Deploy โค้ด Apps Script (ชุดที่มี DataCheckOut) มาวางตรงนี้
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwN_dP6AshriiNt9auC1LKD-oCgdRKbpwNqcGpJa13YpXDPhoaNOCMw2jyp776U6uNysQ/exec';
-
-// LIFF_ID ต่างๆ
-const LIFF_ID = '2007747842-OGeyrGkv';       // LIFF ID Checkin
-const LIFF_HISTORY = '2007747842-p0YX1oBn';  // LIFF ID history
-const LIFF_REGISTER = '2007747842-oJr1GXK2'; // LIFF ID register
